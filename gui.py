8bd48d879810981238f9a738783c1bd4c8f04a46
Mike <mallisiscool@gmail.com>
2024-09-29 20:29:34 -0400

    Working save uploading and patching

--- gui.py
+++ gui.py
@@ -1,7 +1,56 @@
-from nicegui import ui
+from nicegui import ui, binding
+from gamedata.WeaponType import WeaponType
 import savedata
 
+active_save: savedata.SaveData = savedata.SaveData()
+
+class SlotProperties:
+    weapon_type = binding.BindableProperty()
+    weapon_ammos = binding.BindableProperty()
+
+class SaveProperties:
+    money = binding.BindableProperty()
+    health = binding.BindableProperty()
+    armor = binding.BindableProperty()
+
+    weapons = []
+
+    def __init__(self):
+        for i in range(0, 11):
+            self.weapons.append(SlotProperties())
+
+    def load(self, save: savedata.SaveData):
+        self.money = save.mMoney
+        self.health = (float(save.mHealth) / 127.0) * 100.0
+        self.armor = (float(save.mArmor) / 127.0) * 100.0
+        for i in range(0, 11):
+            self.weapons[i].weapon_type = save.mWeaponTypes[i]
+            self.weapons[i].weapon_ammos = save.mWeaponAmmos[i]
+
+    def save(self):
+        global active_save
+        active_save.mMoney = self.money
+        active_save.mHealth = int((self.health / 100.0) * 127)
+        active_save.mArmor = int((self.armor / 100.0) * 127)
+        for i in range(0, 11):
+            active_save.mWeaponTypes[i] = int(self.weapons[i].weapon_type)
+            active_save.mWeaponAmmos[i] = int(self.weapons[i].weapon_ammos)
+
+save_props = SaveProperties()
+
+def load_save(slot: int):
+    global active_save
+    active_save = savedata.parse_save(slot)
+    save_props.load(active_save)
+
+def patch_save(slot: int):
+    global active_save
+    save_props.save()
+    savedata.save_patched(slot, active_save)
+
 def run_gui():
+    global active_save
+    global money_input
     dark = ui.dark_mode()
     with ui.tabs().classes('w-full') as tabs:
         info = ui.tab('Info')
@@ -13,12 +62,32 @@ def run_gui():
             with ui.row():
                 ui.label("Save file:")
                 slot_radio = ui.radio([0,1], value=1).props('inline')
-                ui.button('Load', on_click=lambda: savedata.parse_save(slot_radio.value))
+                ui.button('Load', on_click=lambda: load_save(slot_radio.value))
+                ui.button('Patch', on_click=lambda: patch_save(slot_radio.value))
             with ui.row():
                 ui.label("Theme:")
                 ui.button('Dark', on_click=dark.enable)
                 ui.button('Light', on_click=dark.disable)
 
-    
+        with ui.tab_panel(player):
+            ui.label("Player Stats").classes('text-lg')
+            ui.number(label="Money").bind_value(save_props, 'money')
+            ui.separator()
+            with ui.row().classes('w-full'):
+                ui.label("Health")
+                health_slider = ui.slider(min=0, max=100, step=1).bind_value(save_props, 'health')
+                ui.label().bind_text_from(health_slider, 'value')
+            ui.separator()
+            with ui.row().classes('w-full'):
+                ui.label("Armor")
+                armor_slider = ui.slider(min=0, max=100, step=1).bind_value(save_props, 'armor')
+                ui.label().bind_text_from(armor_slider, 'value')
+
+            ui.label("Player Inventory").classes('text-lg')
+            for i in range(0, 11):
+                with ui.row():
+                    ui.label(f"Slot {i}")
+                    weapon_select = ui.select({j.value: j.name for j in WeaponType}).bind_value(save_props.weapons[i], 'weapon_type')
+                    ui.number(label="Ammo").bind_value(save_props.weapons[i], 'weapon_ammos')
 
     ui.run()
--- savedata.py
+++ savedata.py
@@ -1,11 +1,14 @@
 from binaryreader import BinaryReader
 from datetime import datetime, timezone
+from gamedata.WeaponType import WeaponType
 import android
 import os
+import struct
 
 remote_saves_dir = "/sdcard/Android/data/com.rockstargames.gtactw/files/"
 data_dir = "data/"
 backups_dir = data_dir + "backups/"
+exports_dir = data_dir + "patched/"
 
 SAVE_HEADER_OFFSET = 0
 SAVE_HEADER_SIZE = 0x44
@@ -36,9 +39,26 @@ def backup_save():
         return
     files = device.shell(f"ls {remote_saves_dir}").replace('\n', '').split("  ")
     for file in files:
-        error = device.pull(f"/sdcard/Android/data/com.rockstargames.gtactw/files/{file}", f"{backup_dir}/{file}.dat")
+        error = device.pull(f"{remote_saves_dir}{file}", f"{backup_dir}/{file}.dat")
         if error != None:
             print(f"Failed to backup {file}: {error}")
+
+def upload_patches():
+    if not android.adb_started():
+        print("ADB HAS NOT CONNECTED, CANNOT MAKE BACKUP!")
+        return
+    device = android.get_device()
+    if not device:
+        print("ADB STARTED, BUT DEVICE NOT CONNECTED. CANNOT MAKE  BACKUP!")
+        return
+    files = device.shell(f"ls {remote_saves_dir}").replace('\n', '').split("  ")
+    for file in files:
+        patched_file = f"{exports_dir}/{file}.dat"
+        if not os.path.exists(patched_file):
+            continue
+        error = device.push(f"{exports_dir}/{file}.dat", f"{remote_saves_dir}{file}")
+        if error != None:
+            print(f"Failed to upload patch {file}: {error}")
 
 def most_recent_backup() -> str :
     most_recent: str = ""
@@ -77,35 +97,67 @@ def split_save(slot):
             for i in range(0, 0x938):
                 sw.write(sf.read(1))
 class SaveData:
-    mVersionStr: str
+    mVersionStr: str = ""
     mSocialClubStamp: int = 0
 
     mMoney: int = 0
+    mHealth: int = 0
     mArmor: int = 0
+
+    mWeaponTypes: list(WeaponType) = []
+    mWeaponAmmos: list = []
 
     def __init__(self):
         pass
 
     def load_header(self, sr: BinaryReader):
         sr.position = SAVE_HEADER_OFFSET + 8
-        mVersionStr = sr.readStringC()
-        print(f"mVersionStr: '{mVersionStr}'")
+        self.mVersionStr = sr.readStringC()
+        print(f"mVersionStr: '{self.mVersionStr}'")
         sr.position = SAVE_HEADER_OFFSET + 0x40
-        mSocialClubStamp = sr.readInt32()
-        print(f"mSocialClubStamp: {mSocialClubStamp}")
+        self.mSocialClubStamp = sr.readInt32()
+        print(f"mSocialClubStamp: {self.mSocialClubStamp}")
 
     def load_code(self, sr: BinaryReader):
-        sr.position = 0x4C
-        mMoney = sr.readInt32()
-        print(f"Money {mMoney}")
+        sr.position = 0x4D
+        self.mMoney = sr.readInt32()
+        print(f"Money {self.mMoney}")
         sr.position = 0xF5
-        mArmor = sr.readInt8()
-        print(f"Armor: {mArmor}")
+        self.mHealth = sr.readInt8()
+        print(f"Health: {self.mHealth}")
+        sr.position = 0xF6
+        self.mArmor = sr.readInt8()
+        print(f"Armor: {self.mArmor}")
 
-        sr.position = 0xf6
         for i in range(0, 0xB):
-            print(f"{i} - {sr.readInt16()}")
+            sr.position = 0xf7 + i
+            typeValue = sr.readInt8()
+            sr.position = 0x63 + i * 2
+            ammoValue = sr.readInt16()
+            typeName = str(typeValue)
+            try:
+                typeName = WeaponType(typeValue).name
+            except:
+                pass
+            self.mWeaponTypes.append(typeValue)
+            self.mWeaponAmmos.append(ammoValue)
+            print(f"{i} - type: {typeName} ammo: {ammoValue}")
 
+    def patch_code(self, pf):
+        pf.seek(0x4D)
+        pf.write(struct.pack('i', self.mMoney))
+        pf.seek(0xF5)
+        pf.write(struct.pack('b', self.mHealth))
+        pf.seek(0xF6)
+        pf.write(struct.pack('b', self.mArmor))
+
+        for i in range(0, 0xB):
+            pf.seek(0xf7 + i)
+            pf.write(struct.pack('b', self.mWeaponTypes[i]))
+            pf.seek(0x63 + i * 2)
+            pf.write(struct.pack('h', self.mWeaponAmmos[i]))
+
+        pf.flush()
 
 def parse_save(slot: int) -> SaveData:
     backup = most_recent_backup()
@@ -125,3 +177,24 @@ def parse_save(slot: int) -> SaveData:
     print("Loaded!")
 
     return savedata;
+
+def save_patched(slot: int, modified: SaveData) -> str:
+    backup = most_recent_backup()
+    savefile = backup + f"savegame{slot}.dat"
+    print(f"Patching {savefile}...")
+    if not os.path.exists(exports_dir):
+        os.makedirs(exports_dir)
+    patchedfile = exports_dir + f"savegame{slot}.dat"
+    print("Copying save file...")
+    file_bytes: bytes = None
+    with open(savefile, 'rb') as sf:
+        file_bytes = sf.read()
+    with open(patchedfile, 'wb') as pf:
+        pf.write(file_bytes)
+    print(f"Copied {len(file_bytes)} bytes of data")
+    print("Applying patches...")
+    with open(patchedfile, 'rb+') as pf:
+        modified.patch_code(pf)
+        print("Applied code patches!")
+    print("Patched!")
+    upload_patches()
